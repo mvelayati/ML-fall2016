1c1375b9e3aeae2c5697529bfd8dea6729a29e56
mvelayati <m.velayati@outlook.com>
2016-10-16 13:20:21 -0700

    added the feature to print the predicted labels

--- asgn_2/src/classify.py
+++ asgn_2/src/classify.py
@@ -4,6 +4,8 @@ cs534 Implementation Assignment 2
 """
 
 import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib as mpl
 
 
 class NaiveBayes(object):
@@ -27,7 +29,7 @@ class NaiveBayes(object):
         self.likelihoods = {}
 
         # generate vocubulary on instaintiation
-        print 'Generating vocabulary...'
+        print('Generating vocabulary...')
         self._get_vocabulary()
 
 
@@ -39,13 +41,13 @@ class NaiveBayes(object):
         handy for the Priors and Overfitting part in the assignment
         """
 
-        print 'Retreiving classes...'
+        print('Retreiving classes...')
         self._get_classes()
-        print 'Generating dense features...'
+        print('Generating dense features...')
         self._get_features(model=model)
-        print 'Estimating priors...'
+        print('Estimating priors...')
         self._compute_priors()
-        print 'Estimating likelihoods...'
+        print('Estimating likelihoods...')
         self._compute_likelihoods(alpha, beta, model=model)
 
     def test_classifier(self, model='binomial'):
@@ -55,7 +57,7 @@ class NaiveBayes(object):
 
         pred_labels = []
 
-        print 'Classifying dev set...'
+        print('Classifying dev set...')
         for i in range(len(self.dev_set)):
             if model == 'binomial':
                 doc = self._get_binomial_feature(self.dev_set[i])
@@ -64,6 +66,7 @@ class NaiveBayes(object):
             pred_labels.append(self._predict(doc, model=model))
 
         self.pred_labels = pred_labels
+
 
     def get_confusion_matrix(self):
         """
@@ -87,9 +90,9 @@ class NaiveBayes(object):
 
         cm = (cm/float(len(self.pred_labels)))*100
 
-        print "{:20s} {:20s} {:20s}\n{:20s} {:2.2f} {:2.2f}\n{:20s} {:2.2f} {:2.2f}".format(
-                '', 'True ' + _class[0], 'True '+ _class[1], 'Pred ' + _class[0],
-                cm[0,0], cm[0,1], 'Pred ' + _class[1], cm[1,0], cm[1,1])
+        print("{:20s} {:20s} {:20s}\n{:20s} {:2.2f} {:2.2f}\n{:20s} {:2.2f} {:2.2f}".format(
+                '', 'True ' + _class[0], 'True ' + _class[1], 'Pred ' + _class[0],
+                cm[0,0], cm[0,1], 'Pred ' + _class[1], cm[1,0], cm[1,1]))
 
     def _get_vocabulary(self):
         """
@@ -182,7 +185,7 @@ class NaiveBayes(object):
                 self.likelihoods[_class] = (n+alpha)/float(self.Nk[_class]+beta)
             if model == 'multinomial':
                 self.likelihoods[_class] = (n+alpha)/float(t_words+beta)
-                print self.likelihoods[_class]
+                # print(self.likelihoods[_class])
 
     def _bernoulli(self, doc, _class):
         """
@@ -231,6 +234,37 @@ class NaiveBayes(object):
 
         return pred_class
 
+    def save_pred_labels(self):
+        with open('predicted_labels', 'w') as w:
+            for pred_label in self.pred_labels:
+                w.write(pred_label + '\n')
+
+    def report_accuracy(self):
+        count = 0
+        for i in range(len(self.pred_labels)):
+            if self.pred_labels[i] == self.dev_labels[i]:
+                count += 1
+        accuracy = (count/float(len(self.pred_labels)))*100
+        return accuracy
+
+    def MAP_estimation(self):
+        accuracy_arr = []
+        alpha_coef = np.arange(0.00001, 1, 0.00001)
+        for coef in alpha_coef:
+            self.train_classifier(model='multinomial', alpha=1, beta=multi.V_card*coef)
+            self.test_classifier(model='multinomial')
+            accu = self.report_accuracy()
+            print('accuracy = {}'.format(accu))
+            accuracy_arr.append(accu)
+            self.get_confusion_matrix()
+
+        fig = plt.figure(figsize=(5, 4))
+        plt.plot(alpha_coef, accuracy_arr, '-k', label=r'$\rm{Accuracy VS alpha coefficient}$')
+        plt.xscale('log')
+        plt.xlabel(r'alpha coefficient', fontsize=10)
+        plt.ylabel(r'accuracy', fontsize=10)
+        plt.legend(loc='upper right', fontsize=10)
+        fig.savefig("Accuracy_alpha", dpi=200)
 
 if __name__ == '__main__':
 
@@ -256,24 +290,31 @@ if __name__ == '__main__':
     # Bernoulli Model
     # ===============
 
-    bernoulli = NaiveBayes(tweets_train, labels_train, tweets_dev, labels_dev)
-    bernoulli.train_classifier(model='binomial', alpha=1, beta=2)
+    # bernoulli = NaiveBayes(tweets_train, labels_train, tweets_dev, labels_dev)
+    # bernoulli.train_classifier(model='binomial', alpha=1, beta=2)
 
-    bernoulli.test_classifier()
+    # bernoulli.test_classifier()
+    # bernoulli.save_pred_labels()
+    # accu = bernoulli.report_accuracy()
+    # print('accuracy = {}'.format(accu))
 
     # performance
-    bernoulli.get_confusion_matrix()
+    # bernoulli.get_confusion_matrix()
 
     # =================
     # Multinomial Model
     # =================
 
+
     multi = NaiveBayes(tweets_train, labels_train, tweets_dev, labels_dev)
-    multi.train_classifier(model='multinomial', alpha=1, beta=multi.V_card)
+    multi.MAP_estimation()
+    # multi.train_classifier(model='multinomial', alpha=1, beta=multi.V_card)
 
-    multi.test_classifier(model='multinomial')
-
+    # multi.test_classifier(model='multinomial')
+    # multi.save_pred_labels()
+    # accu = multi.report_accuracy()
+    # print('accuracy = {}'.format(accu))
     # performance
-    multi.get_confusion_matrix()
+    # multi.get_confusion_matrix()
 
 
