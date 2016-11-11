aa65096af51dacab2c97d02fccf1fc639233553a
lucaswells <lawells36@gmail.com>
2016-11-11 11:03:24 -0800

    part 2 update

--- asgn_3/src/dtree.py
+++ asgn_3/src/dtree.py
@@ -21,13 +21,6 @@ class TrainTree(object):
         # stopping criteria
         self.k = k
 
-        # train tree
-        self.tree = self.train(self.x, self.y)
-
-        self.missed = 0.0
-        self.get_missed(self.tree)
-        self.accuracy = (1 - (self.missed/len(self.x[0])))*100
-
     def _entropy(self, y):
         """
         Compute entropy from target values: H(y) = -sum(pi*log2(pi))
@@ -113,14 +106,14 @@ class TrainTree(object):
 
         # if y only has 1 label then return it
         if len(set(y)) == 1:
-            rv = [0,0,0]
+            rv = np.zeros(len(np.unique(self.y)))
             i = int(y[0])
             rv[i] = len(y)
             return rv
 
         # if number of instances in y is less than k then return y
         if len(y) < self.k:
-            rv = [0,0,0]
+            rv = np.zeros(len(np.unique(self.y)))
             u,c = np.unique(y, return_counts=True)
             for i in range(len(c)):
                 rv[int(u[i])] = c[i]
@@ -145,7 +138,7 @@ class TrainTree(object):
 
         # if there is no gain then return y
         if I <= 0:
-            rv = [0,0,0]
+            rv = np.zeros(len(np.unique(self.y)))
             i = int(y[0])
             rv[i] = len(y)
             return rv
@@ -157,47 +150,122 @@ class TrainTree(object):
 
         return tree
 
+    def get_accuracy(self, tree):
 
-    def get_missed(self, tree):
+        self.missed = 0.0
+        self._get_missed(tree)
+        accuracy = (1 - (self.missed/len(self.x[0])))*100
+        return accuracy
+
+
+    def _get_missed(self, tree):
         """
         Sum the total number of missclassifications
         """
 
         for v in tree.values():
             if isinstance(v, dict):
-                self.get_missed(v)
+                self._get_missed(v)
             else:
                 if np.count_nonzero(v) > 1:
-                    self.missed += v[v!=0].min()
+                    self.missed += v[np.where(v != 0)].min()
+
+
+class RandomForest(TrainTree):
+
+    def __init__(self, train_csv, k, L):
+        """
+        Subclass of TrainTree. Builds L number of trees
+        """
+        # call super constructor
+        super(RandomForest, self).__init__(train_csv, k)
+
+        self.forest = []
+        self.accuracy = []
+
+        for i in range(0, L):
+            x,y = self.randomize_wr(self.x, self.y, len(self.x[0]))
+            tree = self.train(x, y, feature_bagging=True)
+            self.forest.append(tree)
+        for tree in self.forest:
+            self.accuracy.append(self.get_accuracy(tree))
+        self.accuracy = np.mean(self.accuracy)
+
+    def randomize_wr(self, x, y, n):
+        """
+        Generates a random dataset of size n drawing with replacement from
+        original dataset
+        """
+
+        xr = np.zeros((len(x), n))
+        yr = np.zeros(n)
+        for s in range(0, n):
+            r = random.randint(0, len(x[0])-1)
+            xr[:,s] = np.copy(x[:,r])
+            yr[s] = np.copy(y[r])
+
+        return xr, yr
+
 
 class TestTree(object):
 
-    def __init__(self, train_csv, trained_tree):
+    def __init__(self, test_csv, tree):
         """ Constructor """
 
-        data = np.genfromtxt(train_csv, delimiter=';')
+        data = np.genfromtxt(test_csv, delimiter=';')
         self.x = data[:,0:4].T
         self.y = data[:,4].astype(np.int)
 
-        self.yhat = self.test(trained_tree)
-
         missed = 0.0
-        for i in range(len(self.y)):
-            if self.y[i] != self.yhat[i]:
-                missed += 1
-        self.accuracy = (1 - (missed/len(self.y)))*100
+        if isinstance(tree, dict):
+            self.yhat = self.test(tree)
 
-    def test(self, trained_tree):
+            for i in range(len(self.y)):
+                if self.y[i] != self.yhat[i]:
+                    missed += 1
+            self.accuracy = (1 - (missed/len(self.y)))*100
+        else:
+            yhat = np.zeros((len(tree), len(self.y)))
+
+            for i in range(len(tree)):
+                yhat[i] = self.test(tree[i])
+            yhat_mv = self.majority_vote(yhat)
+            for i in range(len(self.y)):
+                if self.y[i] != yhat_mv[i]:
+                    missed += 1
+            self.accuracy = (1 - (missed/len(self.y)))*100
+
+    def test(self, tree):
+        """
+        Classify each feature in the test set base on trained tree
+        """
 
         x = self.x
 
         yhat = []
         for i in range(len(x[0])):
-            yhat.append(self.classify_sample(x[:,i], trained_tree))
+            yhat.append(self.classify_sample(x[:,i], tree))
 
         return np.array(yhat)
 
+    def majority_vote(self, yhat):
+        """
+        Majority voting for all trees
+        """
+
+        yhat_mv = np.zeros(len(yhat[0]))
+        for i in range(len(yhat[0])):
+            u,c = np.unique(yhat[:,i], return_counts=True)
+            yhat_mv[i] = u[int(np.argmax(c))]
+
+        return yhat_mv
+
+
+
     def classify_sample(self, x, tree):
+        """
+        Classify specified sample
+        """
 
         Q = []
         Q.append(tree)
@@ -205,7 +273,7 @@ class TestTree(object):
             sub_tree = Q.pop(0)
             for k in sub_tree.keys():
                 if eval(k):
-                    if isinstance(sub_tree[k], list):
+                    if isinstance(sub_tree[k], np.ndarray):
                         return np.argmax(sub_tree[k])
                     else:
                         Q.append(sub_tree[k])
@@ -213,29 +281,55 @@ class TestTree(object):
 
 if __name__ == '__main__':
 
-    train = TrainTree('data/iris_train.csv', 10)
-    pprint(train.tree)
-    test = TestTree('data/iris_test.csv', train.tree)
-    print test.accuracy
-
     # PART 1
-
     # iterate k
     train_acc = []
     test_acc = []
     for i in range(0, 100):
         train = TrainTree('data/iris_train.csv', i)
-        test = TestTree('data/iris_test.csv', train.tree)
-        train_acc.append(train.accuracy)
+        tree = train.train(train.x, train.y)
+        test_accuracy = train.get_accuracy(tree)
+        test = TestTree('data/iris_test.csv', tree)
+        train_acc.append(test_accuracy)
         test_acc.append(test.accuracy)
 
+    # plot training and testing error over k
     fig = plt.figure()
     plt.plot(train_acc, '-k', label='train')
     plt.plot(test_acc, '--k', label='test')
     plt.legend(loc='lower right')
-    plt.ylim((0,110))
+    plt.ylim((50,105))
     plt.xlabel("$k$")
     plt.ylabel("Accuracy (%)")
     fig.savefig('iter_k.png')
 
-   # PART 2
+    # PART 2
+    L = [5,10,15,20,25,30]
+    K = range(0,100)
+    N = 10
+    for l in L:
+        train_acc_k = []
+        test_acc_k = []
+        for k in K:
+            train_acc = []
+            test_acc = []
+            for n in range(N):
+                rf = RandomForest('data/iris_train.csv', k, l)
+                test = TestTree('data/iris_train.csv', rf.forest)
+                train_acc.append(rf.accuracy)
+                test_acc.append(test.accuracy)
+            print np.mean(train_acc), np.mean(test_acc)
+            train_acc_k.append(np.mean(train_acc))
+            test_acc_k.append(np.mean(test_acc))
+
+        # for each L, plot testing and training error over k
+        fig = plt.figure()
+        plt.plot(train_acc_k, '-k', label='train')
+        plt.plot(test_acc_k, '--k', label='test')
+        plt.legend(loc='lower right')
+        plt.title("$L$={0}".format(l))
+        plt.ylim((50,105))
+        plt.xlabel("$k$")
+        plt.ylabel("Accuracy (%)")
+        fig.savefig('L_{0}.png'.format(l))
+
